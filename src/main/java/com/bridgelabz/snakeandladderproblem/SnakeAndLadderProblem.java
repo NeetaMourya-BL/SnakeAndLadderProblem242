0594b607cb635b5b24ae24f8d9543e88edc42628
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2023-01-11 20:57:34 +0530

    Welcome to Snake and ladder problem

---
+++ src/main/java/com/bridgelabz/snakeandladderproblem/SnakeAndLadderProblem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeandladderproblem;
+
+public class SnakeAndLadderProblem {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake and ladder problem");
+    }
+}
